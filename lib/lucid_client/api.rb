e1d6d707912e2491bb344498a926aa970a87a2d8
Kelsey Judson <kelseyjudson@gmail.com>
2014-01-23 14:53:05 +1300

    Improve resource mappings and models.

--- lib/lucid_client/api.rb
+++ lib/lucid_client/api.rb
@@ -12,13 +12,37 @@ module LucidClient::API
     end
   end
 
-  def represent( model, resource )
-    LucidClient::Resource.new( resource ).as( model )
+  ### Resource Mapping
+
+  def represent( resource, model = _model )
+    represent_as( resource, model ) do
+      LucidClient::Resource.new( resource ).as( model )
+    end
   end
 
-  def represent_each( model, resources )
-    LucidClient::Resource.map( model, resources )
+  def represent_each( resources, model = _model )
+    represent_as( resources, model ) do
+      LucidClient::Resource.map( model, resources )
+    end
   end
+
+  # Only map to resource is the given model implements +LucidClient::Model+.
+  # You might want to set these models as a config setting to avoid hard
+  # coding it into your interfaces, eg.
+  #
+  #     represent_each( LucidClient.config[:shop_model], resources )
+  #
+  def represent_as( resource, model = _model, &block )
+    is_model?( model ) ? block.call( resource ) : resource
+  end
+
+  def is_model?( model )
+    if model.respond_to?( :ancestors )
+      model.ancestors.include?( LucidClient::Model )
+    end
+  end
+
+  ### Request Parameters
 
   # In most casts, override with +super.merge( ... )+.
   #
@@ -29,6 +53,16 @@ module LucidClient::API
   # A +nil+ for +fields+ returns all fields.
   #
   def _fields
+    model_fields
+  end
+
+  def model_fields( options = {} )
+    model = options[:model] || _model
+
+    is_model?( model ) ? model.fields : options[:default]
+  end
+
+  def _model
   end
 
   def session
--- lib/lucid_client/mixins/model.rb
+++ lib/lucid_client/mixins/model.rb
@@ -1,13 +1,39 @@
 module LucidClient::Model
 
-  # A hash of mappings between model attributes and API resource hash values.
-  #
-  def resource_mappings
-    raise NotImplementedError, 'Implement the LucidClient::Model interface'
+  def self.included( base )
+    base.extend( ClassMethods )
   end
 
-  def fields
-    resource_mappings.values.join( ',' )
+  module ClassMethods
+
+    # Maps local attributes to properties of a remote resource. The block
+    # passed to this method is expected to return a hash of mappings.
+    #
+    # Accessor methods for each local attribute will be created, so call this
+    # before defining any accessor overrides.
+    #
+    def map_resources( &block )
+      unless ( mappings = block.call ).kind_of?( Hash )
+        raise 'Block in #map_resources must return a Hash'
+      end
+
+      attr_accessor *mappings.keys
+
+      define_singleton_method( :resource_mappings ) do
+        mappings
+      end
+    end
+
+    # A hash of mappings between model attributes and API resource hash values.
+    #
+    def resource_mappings
+      raise NotImplementedError, 'Implement the LucidClient::Model interface'
+    end
+
+    def fields
+      resource_mappings.values.join( ',' )
+    end
+
   end
 
 end
