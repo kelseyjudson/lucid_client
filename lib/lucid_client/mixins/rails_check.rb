9e4ef8ba7dacc27a42114f0e66801c2174239640
Kelsey Judson <kelseyjudson@gmail.com>
2014-01-30 21:47:50 +1300

    RailsCheck ensures table exists (avoids initialization issues prior to migrations).

--- lib/lucid_client/mixins/rails_check.rb
+++ lib/lucid_client/mixins/rails_check.rb
@@ -7,7 +7,15 @@ module LucidClient::RailsCheck
   def active_record?( model = nil )
     ar = defined?( ::ActiveRecord )
 
-    model && ar ? model.ancestors.include?( ::ActiveRecord::Base ) : ar
+    model && ar ? active_record_model?( model ) : ar
+  end
+
+  private
+
+  def active_record_model?( model )
+    subclass = model.ancestors.include?( ::ActiveRecord::Base )
+
+    subclass && model.table_exists?
   end
 
 end
